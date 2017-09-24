0a405d552f1e7f32cfface16418daa2cd4acbda5
Annalise Ko <annaliseliuko@gmail.com>
2017-09-24 13:52:55 -0700

    HW2 Complete

--- HomeWork/HW2/pageRank/src/main/java/cs181/Mapper1.java
+++ HomeWork/HW2/pageRank/src/main/java/cs181/Mapper1.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.*;
    
 public class Mapper1 extends Mapper<LongWritable, Text, Text, Text> {      
 		
-	/* TODO - Implement the map function, where each call to the function receives just 1 line from the input files. 
+	/* The map function, where each call to the function receives just 1 line from the input files. 
 	 * Recall, we had two input files feed-in to our map reduce job, both the adjacency matrix and the vector file. 
 	 * This, our code must contain some logic to differentiate between the two inputs, and output the appropriate key-value pair.
 	 * 
@@ -16,8 +16,8 @@ public class Mapper1 extends Mapper<LongWritable, Text, Text, Text> {
 	 * 			  Vector Format					->	V  \t  j	\t  value 
 	 * 
 	 * Output :   Key-Value Pairs               
-	 * 			  Key ->   	i
-	 * 			  Value -> 	M 	\t 	j 	\t 	value    or   
+	 * 			  Key ->   	j
+	 * 			  Value -> 	M 	\t 	i 	\t 	value    or   
 	 * 						V 	\t  value 
 	 */
 
@@ -29,9 +29,18 @@ public class Mapper1 extends Mapper<LongWritable, Text, Text, Text> {
 
 		Text outputKey = new Text();
 		Text outputValue = new Text();
-
-		// TODO
 		
-		// Note, use 'context.write (outputKey, outputValue) to output a key-value pair 
+		/* If the value is from the matrix: Tuple is ("M", i, j, value) */
+		if (indicesAndValue[0].equals("M")) {
+			outputKey.set(  indicesAndValue[2]  ); // key is j
+			outputValue.set( "M\t" + indicesAndValue[1] + "\t" + indicesAndValue[3] );  // value is (i, value)
+		}
+		/* If the value is from the vector: Tuple is ("V", j, value) */
+		else {
+			outputKey.set(  indicesAndValue[1]  ); // key is j
+			outputValue.set( "V\t"+indicesAndValue[2] );  // value is (v_k)	
+		}
+		/* Note, use 'context.write (outputKey, outputValue) to output a key-value pair */
+		context.write(outputKey, outputValue);
 	}
 }
